3588756888127c0e6018783f4dcce418aa6c3f96
Koutheir Elwannassi <koutheir.elwannassi@showpad.com>
2019-02-11 15:01:03 +0100

    Routing Done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from '../app/heroes/heroes.component';
+import { DashboardComponent } from '../app/dashboard/dashboard.component';
+import { HeroDetailComponent } from '../app/hero-detail/hero-detail.component';
+
+
+
+const routes: Routes = [
+    { path: 'detail/:id', component: HeroDetailComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'heroes', component: HeroesComponent }
+];
+
+
+
+
+
+
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
